906d19815eb87d11402f227bffc88a5596d5366e
Gregory Danielson <gregory.danielson3@gmail.com>
2022-11-03 08:30:54 -0500

    Shrink footprint while demonstrating; appease py>=3.10

--- aiohttp_example.py
+++ aiohttp_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import asyncio
 import logging
-from concurrent.futures import ThreadPoolExecutor
+import sys
 
 import aiohttp
 from memory_profiler import profile
@@ -23,7 +23,7 @@ async def grab_data(url, session):
 async def quicktest():
     url = "https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf"
     session = aiohttp.ClientSession()
-    jobs = [grab_data(url, session) for _ in range(5)]
+    jobs = [grab_data(url, session) for _ in range(1)]
     jobs_result = await asyncio.gather(*jobs)
     if not all(jobs_result):  # sanity check
         _log.info("Something didn't give back data")
@@ -33,7 +33,10 @@ async def quicktest():
 @profile
 def main():
     logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
-    loop = asyncio.get_event_loop()
+    if sys.version_info >= (3, 10, 0):
+        loop = asyncio.new_event_loop()
+    else:
+        loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(quicktest())
         # memory usage should be back to normal
